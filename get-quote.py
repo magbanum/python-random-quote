dd9066013aa0bc6eb32540778a8024df3118fffe
magbanum <nighotshantanu@gmail.com>
2020-12-15 14:19:14 +0530

    Completed

--- get-quote.py
+++ get-quote.py
@@ -5,10 +5,12 @@ def master():
   f = open("quotes.txt")
   quotes = f.readlines()
   f.close()
-
+  
   last = len(quotes) - 1
   rnd = random.randint(0, last)
+  #rnd2 = random.randint(0, last)
   print(quotes[rnd])
+  #print(quotes[rnd2])
 
 if __name__== "__main__":
   master()
